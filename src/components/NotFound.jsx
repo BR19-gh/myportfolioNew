896294880105917cfe25d1c94a892cbbeddb7fa5
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2024-02-25 15:25:33 +0300

    feat: adding loading spinner for better ux

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -30,51 +30,54 @@ const NotFound = ({
     handleResize();
 
     window.addEventListener("resize", handleResize);
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
   return (
-    <Container
-      onClick={() => {
-        setExpanded(false);
-      }}
-      className="text-center showPage"
-    >
-      <Image
-        style={{
-          marginTop: fontSize === "lg" ? "-270px" : "100px",
-          marginBottom: fontSize === "lg" ? "-130px" : "-80px",
-          height: fontSize === "lg" ? "700px" : "300px",
-        }}
-        src={err404Img}
-        alt="404 Error"
-      />
-      <div className="text-center">
-        <h1
-          style={{
-            color: myPrimaryColor,
-            fontSize: fontSize === "lg" ? "80px" : "35px",
+    <>
+      {window.location.pathname === "/ar" ||
+      window.location.pathname === "/en" ? (
+        setLoading(true)
+      ) : (
+        <Container
+          onClick={() => {
+            setExpanded(false);
           }}
+          className="text-center showPage"
         >
-          Error 404 خطأ
-        </h1>
-        <p
-          style={{
-            fontSize: fontSize === "lg" ? "30px" : "15px",
-          }}
-        >
-          الصفحة التي تبحث عنها غير موجودة
-          <br />
-          The page you are looking for does not exist
-        </p>
-      </div>
-    </Container>
+          <Image
+            style={{
+              marginTop: fontSize === "lg" ? "-270px" : "100px",
+              marginBottom: fontSize === "lg" ? "-130px" : "-80px",
+              height: fontSize === "lg" ? "700px" : "300px",
+            }}
+            src={err404Img}
+            alt="404 Error"
+          />
+          <div className="text-center">
+            <h1
+              style={{
+                color: myPrimaryColor,
+                fontSize: fontSize === "lg" ? "80px" : "35px",
+              }}
+            >
+              Error 404 خطأ
+            </h1>
+            <p
+              style={{
+                fontSize: fontSize === "lg" ? "30px" : "15px",
+              }}
+            >
+              الصفحة التي تبحث عنها غير موجودة
+              <br />
+              The page you are looking for does not exist
+            </p>
+          </div>
+        </Container>
+      )}
+    </>
   );
 };
 
